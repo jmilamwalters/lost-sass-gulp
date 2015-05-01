f82ab1838f20348084c8b25427fb48f4003cc64a
Cory Simmons <cory@launchboxhq.com>
2015-05-01 12:43:19 -0400

    Init

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var browsersync = require('browser-sync'),
+    gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    jshint = require('gulp-jshint'),
+    plumber = require('gulp-plumber'),
+    postcss = require('gulp-postcss'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    gutil = require('gulp-util'),
+    lost = require('lost');
+
+
+// BrowserSync
+gulp.task('browser-sync', function() {
+  browsersync({
+    server: {
+      baseDir: "./"
+    },
+    notify: false
+  });
+});
+
+
+// Sass
+gulp.task('sass', function() {
+  return gulp.src('src/css/**/*.*')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        gutil.beep();
+        console.log(err);
+      }
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([
+      lost()
+    ]))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browsersync.reload({ stream: true }));
+});
+
+
+// JSHint
+gulp.task('jshint', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/js/'))
+});
+
+
+gulp.watch('src/**/*.*', ['sass', 'jshint']);
+
+gulp.task('default', ['sass', 'jshint', 'browser-sync']);
